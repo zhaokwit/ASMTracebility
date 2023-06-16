101dcf6dbe004043de9b1fb9872c6de96e60c861
zhaokwit <zk4637@gmail.com>
2023-06-16 14:39:58 -0400

    saved

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.example.asmtracebility {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
@@ -10,5 +9,6 @@ module com.example.asmtracebility {
     requires org.apache.poi.ooxml;
 
     opens com.example.asmtracebility to javafx.fxml;
+
     exports com.example.asmtracebility;
 }
